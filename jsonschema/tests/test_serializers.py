ae86c0a13833a9b992716dafcf203ff11530edc7
Laurence Rowe <laurence@lrowe.co.uk>
2013-09-30 17:40:58 -0700

    Fix serialize tests.

--- jsonschema/tests/test_serializers.py
+++ jsonschema/tests/test_serializers.py
@@ -5,13 +5,15 @@ from jsonschema.validators import Draft4Validator
 class SerializeMixin(object):
     def test_it_serializes_default_properties(self):
         schema = {"properties": {"foo": {"default": "bar"}}}
-        result = self.validator_class(schema, serialize=True).serialize({})
+        result, errors = self.validator_class(schema, serialize=True).serialize({})
         self.assertEquals(result, {"foo": "bar"})
+        self.assertEquals(errors, [])
 
     def test_it_serializes_default_properties_in_items(self):
         schema = {"items": {"properties": {"foo": {"default": "bar"}}}}
-        result = self.validator_class(schema, serialize=True).serialize([{}])
+        result, errors = self.validator_class(schema, serialize=True).serialize([{}])
         self.assertEquals(result, [{"foo": "bar"}])
+        self.assertEquals(errors, [])
 
     def test_it_serializes_properties_in_order(self):
         schema = {"properties": OrderedDict([("foo", {}), ("bar", {})])}
@@ -19,9 +21,10 @@ class SerializeMixin(object):
             schema, types={"object": OrderedDict}, serialize=True,
         )
         value = OrderedDict([("bar", 1), ("foo", 2)])
-        result = validator.serialize(value)
+        result, errors = validator.serialize(value)
         self.assertIsInstance(result, OrderedDict)
         self.assertEquals(list(result), ["foo", "bar"])
+        self.assertEquals(errors, [])
 
     def test_it_serializes_properties_in_order_with_dict(self):
         schema = {"properties": OrderedDict([("foo", {}), ("bar", {})])}
@@ -29,9 +32,10 @@ class SerializeMixin(object):
             schema, types={"object": (OrderedDict, dict)}, serialize=True,
         )
         value = dict([("bar", 1), ("foo", 2)])
-        result = validator.serialize(value)
+        result, errors = validator.serialize(value)
         self.assertIsInstance(result, OrderedDict)
         self.assertEquals(list(result), ["foo", "bar"])
+        self.assertEquals(errors, [])
 
 
 class SerializeDraft4(SerializeMixin, unittest.TestCase):
